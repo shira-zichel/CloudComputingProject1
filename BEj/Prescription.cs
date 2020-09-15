131212dc3594913aef9c0ce20c0f4d4b70684387
shira <shirazichel@gmail.com>
2020-09-15 20:07:02 +0300

    Fix the master

---
+++ BEj/Prescription.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BE
+{
+     public class Prescription
+    {
+        public int ID { get; set; }
+        public string DragName { get; set; }
+        public float amount { get; set; }
+        public int period { get; set; }
+    }
+}
