4690cd8fe2bebf92a69c77ddd39f4af38dfc348e
shira <shirazichel@gmail.com>
2020-09-26 23:10:15 +0300

    Implement BL and DAL

--- BEj/Doctor.cs
+++ BEj/Doctor.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Text;
+
+
 
 namespace BE
 {
@@ -10,6 +13,8 @@ namespace BE
         public int age { get; set; }
         public string WorkPlace { get; set; }
         public int ID { get; set; }
+        public DoctorLicense License { get; set; }
+
         public override string ToString()
         {
             return $"id:{ID} name:{Name} WorkPlace:{WorkPlace}";
--- BLj/ImplementBL.cs
+++ BLj/ImplementBL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,32 +10,186 @@ namespace BL
 {
     public class ImplementBL
     {
+        DAL.ImplementDAL dal = new DAL.ImplementDAL();
         ///////////////Administrator//////////////
-        void UpdateAdministrator(Administrator administrator) { }
-        void addAdministrator(Administrator administrator) { }
-        void deleteAdministrator(Administrator administrator) { }
-        IEnumerable<Administrator> getAllAdministrators() { return null; }
+        void UpdateAdministrator(Administrator administrator)
+        {
+            try
+            {
+                dal.UpdateAdministrator(administrator);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            } 
+        }
+        void addAdministrator(Administrator administrator)
+        {
+            try
+            {
+                dal.addAdministrator(administrator);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        void deleteAdministrator(Administrator administrator) 
+        {
+            dal.deleteAdministrator(administrator);
+        }
+        IEnumerable<Administrator> getAllAdministrators()
+        {
+            return dal.getAllAdministrators(); 
+        }
 
         //////////Doctor//////////////////////
-        void UpdateDoctor(Doctor doctor) { }
-        void AddDoctor(Doctor doctor) { }
-        void deleteDoctor(Doctor doctor) { }
-        IEnumerable<Doctor> getAllDoctors() { return null; }
+        void UpdateDoctor(Doctor doctor)
+        {
+            try
+            {
+                dal.UpdateDoctor(doctor);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+        void AddDoctor(Doctor doctor)
+        {
+            try
+            {
+                dal.AddDoctor(doctor);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        void deleteDoctor(Doctor doctor) 
+        {
+            try
+            {
+                dal.deleteDoctor(doctor);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        IEnumerable<Doctor> getAllDoctors()
+        {
+            return dal.getAllDoctors(); 
+        }
 
         ///////////Medicine////////////////
-        void UpdateMedicine(Medicine medicine) { }
-        void AddMedicine(Medicine medicine) { }
-        void deleteMediciner(Medicine medicine) { }
-        IEnumerable<Medicine> getAllMedicines() { return null; }
+        void UpdateMedicine(Medicine medicine)
+        {
+            try
+            {
+                dal.UpdateMedicine(medicine);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        void AddMedicine(Medicine medicine)
+        {
+            try
+            {
+                dal.AddMedicine(medicine);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        void deleteMediciner(Medicine medicine) 
+        {
+            try
+            {
+                dal.deleteMediciner(medicine);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        IEnumerable<Medicine> getAllMedicines() 
+        { 
+            return dal.getAllMedicines(); 
+        }
 
         //////////Patient////////////////
-        void UpdatePatient(Patient patient) { }
-        void AddPatient(Patient patient) { }
-        void deletePatient(Patient patient) { }
-        IEnumerable<Patient> getAllPatients() { return null; }
+        void UpdatePatient(Patient patient)
+        {
+            try
+            {
+                dal.UpdatePatient(patient);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        void AddPatient(Patient patient)
+        {
+            try
+            {
+                dal.AddPatient(patient);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        void deletePatient(Patient patient)
+        {
+            try
+            {
+                dal.deletePatient(patient);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        IEnumerable<Patient> getAllPatients()
+        {
+            return dal.getAllPatients();
+        }
 
         /////////////////Prescription//////////
-        void AddPrescription(Prescription prescription) {  }
-        IEnumerable<Prescription> getAllPrescriptions() { return null; }
+        void AddPrescription(Prescription prescription)
+        { 
+            if(prescription.ReferringDoctor.License.ExpirationDate.Date<DateTime.Today.Date)
+            {
+                throw new Exception("Invalid doctor license");
+            }
+            //check if the medicines don't conflict
+
+            //more checking
+            try
+            {
+                dal.AddPrescription(prescription);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        IEnumerable<Prescription> getAllPrescriptions()
+        { 
+            return dal.getAllPrescriptions(); 
+        }
     }
 }
